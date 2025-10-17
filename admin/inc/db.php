adb7c8dc13aa1ae05bf6f9c0de70a89173d7de29
Shohanur Rahman Shohan <shohanurrahmanshohan.cs@gmail.com>
2025-10-17 22:44:11 +0600

    News blog portal db connection

--- admin/inc/db.php
+++ admin/inc/db.php
@@ -1,5 +1,5 @@
 <?php  
-	$db = mysqli_connect("localhost", "root", "", "project");
+	$db = mysqli_connect("localhost", "root", "", "news_blog_portal");
 
 	if ($db) {
 		// echo "Database Connection Successfully";
